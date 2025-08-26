e9350cd941b232afd291ae24fb496294b59c49bc
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2025-08-26 10:55:48 -0500

    Add correct tests

--- go/day10/main_test.go
+++ go/day10/main_test.go
@@ -1,27 +1,58 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
-var expected = map[string]int{
-	"d": 72,
-	"e": 507,
-	"f": 492,
-	"g": 114,
-	"h": 65412,
-	"i": 65079,
-	"x": 123,
-	"y": 456,
+func TestCycle(t *testing.T) {
+	testCases := []struct {
+		puzzle_input string
+		times        int
+		expected     string
+	}{
+		{"1", 1, "11"},
+		{"11", 1, "21"},
+		{"21", 1, "1211"},
+		{"1211", 1, "111221"},
+		{"111221", 1, "312211"},
+		{"1", 5, "312211"},
+		{"1", 4, "111221"},
+		{"1", 3, "1211"},
+		{"1", 2, "21"},
+	}
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("%v_should_be_%v", tc.puzzle_input, tc.expected), func(t *testing.T) {
+			result := cycle(tc.puzzle_input, tc.times)
+			if result != tc.expected {
+				t.Errorf("got %v; want %v", result, tc.expected)
+			}
+		})
+	}
 }
 
-func TestSolePart1ExampleData(t *testing.T) {
-	wires, instructions := parse("./example.txt")
-	wire_values := solve(wires, instructions)
-
-	for wire, value := range expected {
-		if value != wire_values[wire] {
-			t.Errorf("got %d; want %d", wire_values[wire], value)
-		}
+func TestSolution(t *testing.T) {
+	testCases := []struct {
+		puzzle_input string
+		times        int
+		expected     int
+	}{
+		{"1", 1, 2},
+		{"11", 1, 2},
+		{"21", 1, 4},
+		{"1211", 1, 6},
+		{"111221", 1, 6},
+		{"1", 5, 6},
+		{"1", 4, 6},
+		{"1", 3, 4},
+		{"1", 2, 2},
+	}
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("%v_should_be_%d", tc.puzzle_input, tc.expected), func(t *testing.T) {
+			result := solution(tc.puzzle_input, tc.times)
+			if result != tc.expected {
+				t.Errorf("got %d; want %d", result, tc.expected)
+			}
+		})
 	}
 }
